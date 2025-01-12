7071d252f0df1d5aa4a1e7d5843be40f0545cb77
RoyBae <52058395+baeSoft@users.noreply.github.com>
2025-01-12 23:57:42 +0900

    feat: update data preprocessing dir path

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,7 +7,7 @@ import glob
 import os
 
 # 📂 최신 JSON 파일 탐색 함수
-def find_latest_json(directory='/shared-data'):
+def find_latest_json(directory='./shared-data'):
     json_files = glob.glob(os.path.join(directory, '*.json'))
     if not json_files:
         raise FileNotFoundError("❌ JSON 파일을 찾을 수 없습니다.")
