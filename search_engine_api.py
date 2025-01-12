4e2cf25e2d76d76c49fa873d79a3e8055170415b
RoyBae <52058395+baeSoft@users.noreply.github.com>
2025-01-13 00:12:37 +0900

    feat: update metadata.pkl path

--- search_engine_api.py
+++ search_engine_api.py
@@ -1,9 +1,13 @@
+import os
 from fastapi import FastAPI
 from typing import List
 import pickle
 
+# 절대 경로로 car_metadata.pkl 파일 로드
+file_path = os.path.join(os.path.dirname(__file__), 'shared-data', 'car_metadata.pkl')
+
 # 중고차 데이터 로드
-with open('car_metadata.pkl', 'rb') as f:
+with open(file_path, 'rb') as f:
     car_metadata = pickle.load(f)
 
 # FastAPI 앱 생성
@@ -21,8 +25,8 @@ async def search_cars(budget: int):
     예산 이하의 차량을 검색하는 API
     - 예산(budget)은 만원 단위로 입력받음
     """
-    results = [car for car in car_metadata if car['price'] <= (budget/3)]
-    
+    results = [car for car in car_metadata if car['price'] <= (budget / 3)]
+
     if not results:
         return {"message": "No cars found within the budget"}
 
